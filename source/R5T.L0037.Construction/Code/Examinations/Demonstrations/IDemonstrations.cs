e3b5ff88e29f1842c79f46e4d14eb3b9c0493cda
davidcoats <david@getsafetycone.com>
2024-09-22 20:20:11 -0700

    Interim commit.

--- source/R5T.L0037.Construction/Code/Examinations/Demonstrations/IDemonstrations.cs
+++ source/R5T.L0037.Construction/Code/Examinations/Demonstrations/IDemonstrations.cs
@@ -31,11 +31,11 @@ namespace R5T.L0037.Construction
         {
             /// Inputs.
             var solutionFilePath =
-                @"C:\Code\DEV\Git\GitHub\davidcoats\D8S.S0000.Private\source\D8S.S0000.W000.Private.sln"
+                @"C:\Code\DEV\Git\GitHub\davidcoats\D8S.Z0009\source\D8S.Z0009.sln"
                 .ToSolutionFilePath()
                 ;
             var projectName =
-                "D8S.S0000.W000.L000"
+                "D8S.Z0009.T001"
                 .ToProjectName()
                 ;
             var projectDescription =
@@ -44,7 +44,7 @@ namespace R5T.L0037.Construction
                 //"System-typed, contextually named (refined), derived (with fundamental allowed), values for the R5T.Z0070 values catch-all library."
                 //"Leaf strong-typed, contextually named (refined), derived (with fundamental allowed) values for the R5T.Z0063 output directory path and output file path values library."
                 //"Server-side Razor Components catch-all library project for the D8S.W0006 personal, internal, tech blog project."
-                "Shared library project for the D8S.S0000.W000 Blazor client and server projects."
+                "Member implementations for example identity strings from MS Docs."
                 .ToProjectDescription()
                 ;
             var repositoryUrl = new IsSet<IRepositoryUrl>();
